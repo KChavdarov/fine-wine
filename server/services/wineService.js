748ed53dfafb21c71bec06626d0fd6e87485db55
KChavdarov <k.chavdarov92@gmail.com>
2021-11-21 23:24:40 +0200

    fixed broken export statements in wine service

--- server/services/wineService.js
+++ server/services/wineService.js
@@ -1,5 +1,3 @@
-import {findOne} from '../models/Wine.js';
-
 const Wine = require('../models/Wine.js');
 
 module.exports = {
@@ -9,22 +7,22 @@ module.exports = {
     deleteOne,
 };
 
-export async function getAll() {
+async function getAll() {
     return Wine.find({isDeleted: false});
 }
 
-export async function getOne(id) {
+async function getOne(id) {
     return Wine.findOne({id, isDeleted: false});
 }
 
-export async function create(data) {
+async function create(data) {
     const wine = new Wine(data);
     return wine.save();
 }
 
-export async function deleteOne(id) {
+async function deleteOne(id) {
     try {
-        const wine = await findOne(id);
+        const wine = await getOne(id);
         wine.isDeleted = true;
         return wine.save();
     } catch (error) {
