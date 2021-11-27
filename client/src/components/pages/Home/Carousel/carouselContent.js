f9065cc048e9b823cc3b6180e3169b0dde74a72e
KChavdarov <k.chavdarov92@gmail.com>
2021-11-27 02:44:13 +0200

    added home page carousel

---
+++ client/src/components/pages/Home/Carousel/carouselContent.js
@@ -0,0 +1,32 @@
+export const carouselContent = [
+    {
+        image: 'grapes.jpg',
+        title: 'Get a taste of our exquisite ruby red wines selection',
+        text: 'Explore catalogue',
+        link: '/catalogue',
+    },
+    {
+        image: 'gold-grapes.jpg',
+        title: 'Dive into an endless ocean of gold sensations',
+        text: 'Explore catalogue',
+        link: '/catalogue',
+    },
+    {
+        image: 'rose.jpg',
+        title: 'Enjoy countless moments with our premium rose offering',
+        text: 'Explore catalogue',
+        link: '/catalogue',
+    },
+    {
+        image: 'corks2.jpg',
+        title: 'Make your celebrations truly special with our sparkling wines',
+        text: 'Explore catalogue',
+        link: '/catalogue',
+    },
+    {
+        image: 'four-glasses.jpg',
+        title: 'Find the best companion for your sweet desserts',
+        text: 'Explore catalogue',
+        link: '/catalogue',
+    },
+];
