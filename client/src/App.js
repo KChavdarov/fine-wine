b7405ccd9c7033a2b757e09af099684dcc002f24
KChavdarov <k.chavdarov92@gmail.com>
2021-12-02 00:19:49 +0200

    created app footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {Header} from './components/Header/Header';
 import {Home} from './components/pages/Home/Home';
 import {Login} from './components/Login';
 import {Register} from './components/Register';
+import {Footer} from './components/Footer/Footer';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         </Routes>
       </main>
 
+      <Footer />
     </div>
   );
 }
