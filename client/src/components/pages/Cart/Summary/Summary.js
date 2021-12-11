1f5adb98060d0b9fa00c36444537d88d140a700b
KChavdarov <k.chavdarov92@gmail.com>
2021-12-12 00:58:19 +0200

    added empty cart message

--- client/src/components/pages/Cart/Summary/Summary.js
+++ client/src/components/pages/Cart/Summary/Summary.js
@@ -7,28 +7,35 @@ import {CartContext} from '../Cart';
 export function Summary() {
     const {cartDetails, cartTotal} = useContext(CartContext);
 
+    const content = cartDetails.length === 0
+        ? <article className="empty-cart">
+            <h3 className="heading">Your cart is currently empty</h3>
+            <Link className='main-text' to="/catalogue">Please have a look at our catalogue and select the best wine for your occasion</Link>
+            <Link className='button catalogue-button' to="/catalogue">Catalogue</Link>
+        </article>
+        : <div className="cart-container">
+            <div className='table-header'>
+                <p className='product'>Product</p>
+                <p className='price'>Price</p>
+                <p className='quantity'>Quantity</p>
+                <p className='item-total'>Item Total</p>
+            </div>
+            <div className='table-body'>
+                {cartDetails.map(({wine, quantity}) => <CartItem key={wine._id} wine={wine} quantity={quantity} />)}
+            </div>
+            <div className='table-footer'>
+                <div className="subtotal">
+                    <div className="subtotal-label">Subtotal:</div>
+                    <div className="subtotal-value">&euro;{cartTotal.toLocaleString()}</div>
+                </div>
+                <Link className='button checkout-button' to="/cart/checkout">Proceed to checkout</Link>
+            </div>
+        </div>;
+
     return (
         <section className="page cart container">
             <h1 className="page-title">Shopping Cart</h1>
-
-            <div className="cart-container">
-                <div className='table-header'>
-                    <p className='product'>Product</p>
-                    <p className='price'>Price</p>
-                    <p className='quantity'>Quantity</p>
-                    <p className='item-total'>Item Total</p>
-                </div>
-                <div className='table-body'>
-                    {cartDetails.map(({wine, quantity}) => <CartItem key={wine._id} wine={wine} quantity={quantity} />)}
-                </div>
-                <div className='table-footer'>
-                    <div className="subtotal">
-                        <div className="subtotal-label">Subtotal:</div>
-                        <div className="subtotal-value">&euro;{cartTotal.toLocaleString()}</div>
-                    </div>
-                    <Link className='checkout-button' to="/cart/checkout">Proceed to checkout</Link>
-                </div>
-            </div>
+            {content}
         </section>
     );
 }
