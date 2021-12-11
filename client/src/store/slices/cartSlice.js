377d01bd0e5fb98fbbc46d6fc393dbd87514065d
KChavdarov <k.chavdarov92@gmail.com>
2021-12-11 15:55:57 +0200

    added try-catch

--- client/src/store/slices/cartSlice.js
+++ client/src/store/slices/cartSlice.js
@@ -34,33 +34,49 @@ export const cartReducer = cartSlice.reducer;
 
 export const loadCart = () => {
     return (dispatch) => {
-        const content = localStorage.getItem('cart');
-        if (content) {
-            let cart = JSON.parse(content);
-            dispatch(set(cart));
-        } else {
-            localStorage.setItem('cart', '{}');
+        try {
+            const content = localStorage.getItem('cart');
+            if (content) {
+                let cart = JSON.parse(content);
+                dispatch(set(cart));
+            } else {
+                localStorage.setItem('cart', '{}');
+            }
+        } catch (error) {
+            console.error(error);
         }
     };
 };
 
 export const addItem = (wineId) => {
     return (dispatch, getState) => {
-        dispatch(add(wineId));
-        localStorage.setItem('cart', JSON.stringify(getState().cart));
+        try {
+            dispatch(add(wineId));
+            localStorage.setItem('cart', JSON.stringify(getState().cart));
+        } catch (error) {
+            console.error(error);
+        }
     };
 };
 
 export const subtractItem = (wineId) => {
     return (dispatch, getState) => {
-        dispatch(subtract(wineId));
-        localStorage.setItem('cart', JSON.stringify(getState().cart));
+        try {
+            dispatch(subtract(wineId));
+            localStorage.setItem('cart', JSON.stringify(getState().cart));
+        } catch (error) {
+            console.error(error);
+        }
     };
 };
 
 export const removeItem = (wineId) => {
     return (dispatch, getState) => {
-        dispatch(remove(wineId));
-        localStorage.setItem('cart', JSON.stringify(getState().cart));
+        try {
+            dispatch(remove(wineId));
+            localStorage.setItem('cart', JSON.stringify(getState().cart));
+        } catch (error) {
+            console.error(error);
+        }
     };
 };
