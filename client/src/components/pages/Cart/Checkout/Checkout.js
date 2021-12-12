a63bfc3a0f7eed1fd5a846f2847f03071506c6e1
KChavdarov <k.chavdarov92@gmail.com>
2021-12-12 12:30:19 +0200

    added empty cart guard to checkout

--- client/src/components/pages/Cart/Checkout/Checkout.js
+++ client/src/components/pages/Cart/Checkout/Checkout.js
@@ -4,7 +4,7 @@ import {CartContext} from '../Cart';
 import {useDispatch, useSelector} from 'react-redux';
 import {selectUser} from '../../../../store/slices/userSlice';
 import {toast} from 'react-toastify';
-import {useNavigate} from 'react-router-dom';
+import {Navigate, useNavigate} from 'react-router-dom';
 
 const initialState = {
     firstName: '',
@@ -49,53 +49,55 @@ export function Checkout() {
     }
 
     return (
-        <section className="page checkout container">
-            <h1 className="page-title">
-                Order Checkout
-            </h1>
-            <header className="section-header">
-                <h5>Order Summary</h5>
-            </header>
+        cartDetails.length === 0
+            ? <Navigate to="/cart" replace={true} />
+            : <section className="page checkout container">
+                <h1 className="page-title">
+                    Order Checkout
+                </h1>
+                <header className="section-header">
+                    <h5>Order Summary</h5>
+                </header>
 
 
-            <form className='checkout-form' onSubmit={formSubmitHandler}>
+                <form className='checkout-form' onSubmit={formSubmitHandler}>
 
-                <div className="item-summary">
-                    <h4 className='group-title'>Item summary</h4>
-                    <ul className="item-list">
-                        {itemSummary}
-                    </ul>
-                    <div className="list-total">
-                        <h4 className="order-total-label">Order Total</h4>
-                        <h4 className="order-total-value">&euro;{cartTotal.toLocaleString()}</h4>
+                    <div className="item-summary">
+                        <h4 className='group-title'>Item summary</h4>
+                        <ul className="item-list">
+                            {itemSummary}
+                        </ul>
+                        <div className="list-total">
+                            <h4 className="order-total-label">Order Total</h4>
+                            <h4 className="order-total-value">&euro;{cartTotal.toLocaleString()}</h4>
+                        </div>
                     </div>
-                </div>
 
-                <div className="form-content">
-                    <h4 className='group-title'>Contact Information</h4>
-                    <label htmlFor="firstName">First name</label>
-                    <input type="text" name="firstName" placeholder="John" id="firstName" onChange={inputChangeHandler} value={state.firstName} />
-                    <div className="errors"></div>
+                    <div className="form-content">
+                        <h4 className='group-title'>Contact Information</h4>
+                        <label htmlFor="firstName">First name</label>
+                        <input type="text" name="firstName" placeholder="John" id="firstName" onChange={inputChangeHandler} value={state.firstName} />
+                        <div className="errors"></div>
 
-                    <label htmlFor="lastName">Last name</label>
-                    <input type="text" name="lastName" placeholder="Doe" id="lastName" onChange={inputChangeHandler} value={state.lastName} />
-                    <div className="errors"></div>
+                        <label htmlFor="lastName">Last name</label>
+                        <input type="text" name="lastName" placeholder="Doe" id="lastName" onChange={inputChangeHandler} value={state.lastName} />
+                        <div className="errors"></div>
 
-                    <label htmlFor="email">E-mail</label>
-                    <input type="text" name="email" placeholder="example@email.com" id="email" onChange={inputChangeHandler} value={state.email} />
-                    <div className="errors"></div>
+                        <label htmlFor="email">E-mail</label>
+                        <input type="text" name="email" placeholder="example@email.com" id="email" onChange={inputChangeHandler} value={state.email} />
+                        <div className="errors"></div>
 
-                    <label htmlFor="phone">Phone number</label>
-                    <input type="text" name="phone" placeholder="+359888123123" id="phone" onChange={inputChangeHandler} value={state.phone} />
-                    <div className="errors"></div>
+                        <label htmlFor="phone">Phone number</label>
+                        <input type="text" name="phone" placeholder="+359888123123" id="phone" onChange={inputChangeHandler} value={state.phone} />
+                        <div className="errors"></div>
 
-                    <label htmlFor="address">Address</label>
-                    <input type="text" name="address" id="address" placeholder="City, 1 Example street, ap.1" onChange={inputChangeHandler} value={state.address} />
-                    <div className="errors"></div>
-                </div>
+                        <label htmlFor="address">Address</label>
+                        <input type="text" name="address" id="address" placeholder="City, 1 Example street, ap.1" onChange={inputChangeHandler} value={state.address} />
+                        <div className="errors"></div>
+                    </div>
 
-                <input type="submit" className="button submit-button" value="Place Order" disabled={((status === 'loading') || state.isInvalid)} />
-            </form>
-        </section>
+                    <input type="submit" className="button submit-button" value="Place Order" disabled={((status === 'loading') || state.isInvalid)} />
+                </form>
+            </section>
     );
 }
