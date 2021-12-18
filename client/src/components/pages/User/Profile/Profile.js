e07209b5c15775123479d922455893c6f10c36f9
KChavdarov <k.chavdarov92@gmail.com>
2021-12-18 02:48:14 +0200

    added order fetching in user profile

--- client/src/components/pages/User/Profile/Profile.js
+++ client/src/components/pages/User/Profile/Profile.js
@@ -1,7 +1,36 @@
+import {useState} from 'react';
+import {useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {useEffect} from 'react/cjs/react.development';
+import {useIsAuth} from '../../../../guards/guards';
+import {getOrders} from '../../../../services/orderService';
+import {selectUser} from '../../../../store/slices/userSlice';
 
 export function Profile() {
-    return (
+    const {status, user, errors} = useSelector(selectUser);
+    const [orders, setOrders] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        loadOrders(user._id);
+
+        async function loadOrders(userId) {
+            try {
+                const query = new URLSearchParams();
+                query.append('_creator', user._id);
+                const orders = getOrders(query.toString());
+                setOrders(() => orders);
+            } catch (error) {
+                error.forEach(err => toast.error(err));
+            }
+        }
+    }, [user._id]);
+
+    const isAuth = useIsAuth();
+
+
+    return isAuth(
         <div className="container">
             <Link to="/auth/logout">Logout</Link>
         </div>
--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -6,3 +6,8 @@ export async function createOrder(data) {
     const order = await http.post(endpoint, data);
     return order;
 }
+
+export async function getOrders(query) {
+    const orders = await http.get(endpoint + '?' + query);
+    return orders;
+}
