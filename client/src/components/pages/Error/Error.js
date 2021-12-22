28677ed475e6307601b4e3c6839997035776baf2
KChavdarov <k.chavdarov92@gmail.com>
2021-12-22 02:03:15 +0200

    added error page

--- client/src/components/pages/Error/Error.js
+++ client/src/components/pages/Error/Error.js
@@ -4,9 +4,16 @@ import {Link} from 'react-router-dom';
 export function Error() {
 
     return (
-        <section>
-            <p>Error</p>
-            <Link to="/" >Return Home</Link>
+        <section className="page error container">
+            <h1 className="page-title">Page Error</h1>
+            <header className="section-header">
+                <h4>Looks like we have encountered a problem</h4>
+            </header>
+            <div className='main-text'>
+                <p>Please accept out apologies for the inconvenience caused. Clicking the button below button will lead you back to our home page.</p>
+            </div>
+
+            <Link className='button return' to="/" >Return Home</Link>
         </section >
     );
 }
