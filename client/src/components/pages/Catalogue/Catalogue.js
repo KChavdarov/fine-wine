1d879629fb9c0d8a080f13edb3b0e0815c7f17ce
KChavdarov <k.chavdarov92@gmail.com>
2021-12-29 00:59:44 +0200

    fixed minor bugs and changed year to range input

--- client/src/components/pages/Catalogue/Catalogue.js
+++ client/src/components/pages/Catalogue/Catalogue.js
@@ -6,6 +6,7 @@ import {Filters} from './Filters';
 import {useNavigate, useSearchParams} from 'react-router-dom';
 import {useCallback} from 'react/cjs/react.development';
 import {Paginator} from '../../shared/Paginator/Paginator';
+import {Loader} from '../../shared/Loader/Loader';
 
 let initialFilters = {
     type: {},
@@ -13,19 +14,25 @@ let initialFilters = {
     grape: {},
     country: {},
     region: {},
-    year: {},
     volume: {},
     priceRange: {min: 0, max: 1000},
     minPrice: 0,
     maxPrice: 1000,
+    yearRange: {min: 1900, max: 3000},
+    minYear: 1900,
+    maxYear: 3000,
+};
+
+let initialPaginate = {
     page: 1,
     perPage: 12,
+    count: 0,
     sort: '-isPromo'
 };
 
 export function Catalogue() {
     const [filters, setFilters] = useState(initialFilters);
-    const [products, setProducts] = useState({wines: [], page: 1, perPage: 12, count: 0});
+    const [products, setProducts] = useState({wines: [], ...initialPaginate});
     const [searchParams, setSearchParams] = useSearchParams();
     const [isLoading, setIsLoading] = useState(true);
     const navigate = useNavigate();
@@ -42,12 +49,13 @@ export function Catalogue() {
     const getFilters = useCallback(async (searchParams) => {
         const categories = await wineService.getCategories();
         const selected = parseQueryString(searchParams);
+        const omitFilters = ['minYear', 'maxYear', 'yearRange', 'minPrice', 'maxPrice', 'priceRange', 'isPromo'];
 
         const loadedFilters = Object.entries(categories)
-            .filter(([category, fields]) => (category !== 'minPrice' && category !== 'maxPrice' && category !== 'isPromo'))
+            .filter(([category]) => !(omitFilters.includes(category)))
             .reduce((acc, [category, fields]) => {
                 acc[category] = fields.reduce((a, c) => {
-                    a[c] = Boolean(selected[category]?.includes(c));
+                    a[c] = Boolean(selected[category]?.includes(c.toString()));
                     return a;
                 }, {});
 
@@ -56,13 +64,16 @@ export function Catalogue() {
 
         loadedFilters.minPrice = (selected.minPrice && Number(selected.minPrice[0])) || categories.minPrice;
         loadedFilters.maxPrice = (selected.maxPrice && Number(selected.maxPrice[0])) || categories.maxPrice;
-        if (selected.page) {loadedFilters.page = Number(selected.page[0]);}
-        if (selected.perPage) {loadedFilters.perPage = Number(selected.perPage[0]);}
-        if (selected.sort) {loadedFilters.sort = selected.sort[0];}
-
         loadedFilters.priceRange = {
             min: categories.minPrice,
-            max: categories.maxPrice
+            max: categories.maxPrice,
+        };
+
+        loadedFilters.minYear = (selected.minYear && Number(selected.minYear[0])) || categories.minYear;
+        loadedFilters.maxYear = (selected.maxYear && Number(selected.maxYear[0])) || categories.maxYear;
+        loadedFilters.yearRange = {
+            min: categories.minYear,
+            max: categories.maxYear,
         };
 
         initialFilters = loadedFilters;
@@ -71,10 +82,16 @@ export function Catalogue() {
     }, [parseQueryString]);
 
     const getProducts = useCallback(async (searchParams) => {
+        const page = searchParams.has('page') && Number(searchParams.get('page'));
         const result = await wineService.getAll(searchParams);
+        let pages = Math.ceil(result.count / result.perPage);
+        if (result.count && page > pages) {
+            searchParams.set('page', Math.min(page, pages));
+            setSearchParams(searchParams, {replace: true});
+        };
         setProducts(() => result);
         setIsLoading(false);
-    }, []);
+    }, [setSearchParams]);
 
     const checkboxHandler = useCallback((event, category) => {
         const name = event.target.name;
@@ -94,17 +111,25 @@ export function Catalogue() {
         const name = event.target.name;
 
         setFilters(prevState => {
-            let minPrice = prevState.minPrice;
-            let maxPrice = prevState.maxPrice;
+            const ranges = {
+                minPrice: prevState.minPrice,
+                maxPrice: prevState.maxPrice,
+                minYear: prevState.minYear,
+                maxYear: prevState.maxYear,
+            };
             const newValue = Number(event.target.value);
 
             if (name === 'minPrice') {
-                minPrice = Math.min(maxPrice - 10, newValue);
+                ranges.minPrice = Math.min(ranges.maxPrice - 10, newValue);
             } else if (name === 'maxPrice') {
-                maxPrice = Math.max(minPrice + 10, newValue);
+                ranges.maxPrice = Math.max(ranges.minPrice + 10, newValue);
+            } else if (name === 'minYear') {
+                ranges.minYear = Math.min(ranges.maxYear - 1, newValue);
+            } else if (name === 'maxYear') {
+                ranges.maxYear = Math.max(ranges.minYear + 1, newValue);
             }
 
-            return {...prevState, minPrice, maxPrice};
+            return {...prevState, ...ranges};
         });
     }, []);
 
@@ -118,7 +143,7 @@ export function Catalogue() {
     }, [getFilters, navigate, searchParams, setSearchParams]);
 
     const mapFiltersToQuerystring = useCallback(() => {
-        const omitFilters = ['minPrice', 'maxPrice', 'priceRange', 'page', 'perPage', 'sort'];
+        const omitFilters = ['minYear', 'maxYear', 'yearRange', 'minPrice', 'maxPrice', 'priceRange', 'isPromo'];
         const selectedFilters = Object.entries(filters)
             .filter(([category]) => !(omitFilters.includes(category)))
             .reduce((acc, [category, fields]) => {
@@ -127,19 +152,27 @@ export function Catalogue() {
                 return acc;
             }, {});
 
-        selectedFilters.minPrice = filters.minPrice;
-        selectedFilters.maxPrice = filters.maxPrice;
-        selectedFilters.perPage = Number(filters.perPage);
-        selectedFilters.sort = filters.sort;
-        selectedFilters.page = Number(filters.page);
+        if ((searchParams.has('minPrice') && searchParams.get('minPrice') !== filters.minPrice) || (filters.minPrice !== initialFilters.minPrice)) {
+            selectedFilters.minPrice = filters.minPrice;
+        }
+        if ((searchParams.has('maxPrice') && searchParams.get('maxPrice') !== filters.maxPrice) || (filters.maxPrice !== initialFilters.maxPrice)) {
+            selectedFilters.maxPrice = filters.maxPrice;
+        }
+        if ((searchParams.has('minYear') && searchParams.get('minYear') !== filters.minYear) || (filters.minYear !== initialFilters.minYear)) {
+            selectedFilters.minYear = filters.minYear;
+        }
+        if ((searchParams.has('maxYear') && searchParams.get('maxYear') !== filters.maxYear) || (filters.maxYear !== initialFilters.maxYear)) {
+            selectedFilters.maxYear = filters.maxYear;
+        }
 
         setSearchParams(selectedFilters);
-    }, [filters, setSearchParams]);
+    }, [filters, searchParams, setSearchParams]);
 
     const filtersSubmitHandler = useCallback((event) => {
         event.preventDefault();
         mapFiltersToQuerystring();
     }, [mapFiltersToQuerystring]);
+
 
     const paginationHandler = useCallback((event) => {
         let page = products.page;
@@ -161,10 +194,12 @@ export function Catalogue() {
             setSearchParams(parseQueryString(searchParams));
         }
         if (perPage !== products.perPage) {
+            searchParams.set('page', 1);
             searchParams.set('perPage', perPage);
             setSearchParams(parseQueryString(searchParams));
         }
         if (sort !== products.sort) {
+            searchParams.set('page', 1);
             searchParams.set('sort', sort);
             setSearchParams(parseQueryString(searchParams));
         }
@@ -175,15 +210,14 @@ export function Catalogue() {
         try {
             getFilters(searchParams);
             getProducts(searchParams);
-            return () => setProducts(products => products);
         } catch (error) {
             navigate('/error');
         }
     }, [getFilters, getProducts, navigate, searchParams]);
 
-    const content = isLoading
-        ? <p>Loading ...</p>
-        : products.wines.map(product => <ProductCard key={product._id} product={product} />);
+    const content = products.count
+        ? products.wines.map(product => <ProductCard key={product._id} product={product} />)
+        : <div className="no-results"><p>No wines meet the specified criteria.</p> <p>Please refine your filters.</p></div>;
 
     return (
         <section className="page catalogue container">
@@ -194,9 +228,12 @@ export function Catalogue() {
             {isLoading ? null : <Paginator handler={paginationHandler} products={products} />}
 
             <div className="products-container">
-                {content}
+                {isLoading
+                    ? <Loader />
+                    : content
+                }
             </div>
 
         </section>
     );
-};
+};;
